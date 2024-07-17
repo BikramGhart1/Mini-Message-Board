2f3dccbc0eb93185fe6e59664aeb3d3bf1cc24aa
BikramGhart1 <coludingrain@gmail.com>
2024-07-17 17:42:05 +0545

    adding comments

--- server.js
+++ server.js
@@ -4,14 +4,20 @@ const indexrouter = require('./routes/index');
 const bodyParser = require('body-parser');
 const path = require("path");
 
+//to use ejs files
 app.set('view engine', 'ejs');
 
+//for accessing input fields
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(express.static(__dirname + '/public'));
+//using our route file
 app.use("/", indexrouter);
+//to get json values
 app.use(express.json());
+//for connecting css files
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//listen to port 3000
 app.listen(3000, () => {
     console.log("connected");
 
